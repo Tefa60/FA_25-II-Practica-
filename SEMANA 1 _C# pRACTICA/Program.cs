e7d4df5a7cfe5dcb87e8c0ca5dedf0f2a2f057eb
dannae <dannaeemt@gmail.com>
2025-09-02 15:42:32 -0500

    subiendo ejersicio-sem1

---
+++ SEMANA 1 _C# pRACTICA/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA_1_PRACTICA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejer1();
+            Console.ReadKey();
+        }
+        static void Ejer1()
+        {
+            string nombre, carrera; //declarando variables
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su carrera");
+            carrera = Console.ReadLine();
+            Console.WriteLine($"{nombre} bienbenido al curso fundamentos de algoritmos de la carrera {carrera}");
+
+
+        }
+        static void Ejer2()
+        {
+
+        }
+        static void Ejer3()
+        {
+
+        }
+        static void Ejer4()
+        {
+
+        }
+        static void Ejer5()
+        {
+
+        }
+        static void Ejer6()
+        {
+
+        }
+    }
+}
---
+++ SEMANA 1_ PYTHON/SEMANA_1__PYTHON.py
@@ -0,0 +1,5 @@
+﻿def ejer1():
+    nombre= input("Ingrese su nombrep: ")
+    carrera= input("Ingrese su carrera: ")
+    print(f"{nombre}, bienvenido a FA de { carrera}") 
+ejer1()
